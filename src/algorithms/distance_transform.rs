b3578034e23d7ad978e525a60fafbd0f04eddc2a
Shane Madden <shanemadden@users.noreply.github.com>
2024-05-20 14:20:15 -0600

    Distance transform ergonomic tweaks, changelog (#18)

--- src/algorithms/distance_transform.rs
+++ src/algorithms/distance_transform.rs
@@ -1,26 +1,26 @@
 // Heavily based on https://github.com/Screeps-Tutorials/Screeps-Tutorials/blob/Master/basePlanningAlgorithms/distanceTransform.js
 
-use crate::offline_map;
-
 use screeps::{
     self,
     constants::{extra::ROOM_SIZE, Direction},
-    local::{LocalCostMatrix, RoomXY},
+    local::{LocalCostMatrix, LocalRoomTerrain, RoomXY},
 };
 
 /// Provides a Cost Matrix with values equal to the Chebyshev distance from any
 /// wall terrain. This does *not* calculate based on constructed walls, only
 /// terrain walls.
-pub fn distance_transform(room_data: &offline_map::OfflineRoomData) -> LocalCostMatrix {
+pub fn chebyshev_distance_transform_from_terrain(
+    room_terrain: &LocalRoomTerrain,
+) -> LocalCostMatrix {
     let mut initial_cm = LocalCostMatrix::new();
 
     for (xy, cm_val) in initial_cm.iter_mut() {
-        *cm_val = match room_data.terrain.get_xy(xy) {
+        *cm_val = match room_terrain.get_xy(xy) {
             screeps::constants::Terrain::Wall => 0,
             _ => u8::MAX,
         };
     }
-    distance_transform_from_cost_matrix(initial_cm)
+    chebyshev_distance_transform_from_cost_matrix(initial_cm)
 }
 
 /// Provides a Cost Matrix with values equal to the Chebyshev distance from any
@@ -28,7 +28,9 @@ pub fn distance_transform(room_data: &offline_map::OfflineRoomData) -> LocalCost
 /// allows for calculating the distance transform from an arbitrary set of
 /// positions. Other position values in the initial Cost Matrix should be
 /// initialized to 255 (u8::MAX) to ensure the calculations work correctly.
-pub fn distance_transform_from_cost_matrix(initial_cm: LocalCostMatrix) -> LocalCostMatrix {
+pub fn chebyshev_distance_transform_from_cost_matrix(
+    initial_cm: LocalCostMatrix,
+) -> LocalCostMatrix {
     // Copy the initial cost matrix into the output cost matrix
     let mut cm = initial_cm.clone();
 
