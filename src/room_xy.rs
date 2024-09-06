6d94028c208f6cc38a882debf918f02b57ea6ba6
Ken Hoover <k.hoover3@gmail.com>
2024-09-05 20:56:59 -0700

    Refactor GridIter.
    Split the struct into an iterator over generic RoomCoordinate pairs and
    a part that handles translating pairs into RoomXYs, based on the order.
    Also uses the room_coordinate iterators instead of home-rolled unsafe.

--- src/room_xy.rs
+++ src/room_xy.rs
@@ -1,6 +1,185 @@
 use std::iter::FusedIterator;
 
-use screeps::{Room, RoomCoordinate, RoomXY};
+use screeps::{RoomCoordinate, RoomXY};
+
+use crate::room_coordinate::{range_exclusive, range_inclusive};
+
+#[derive(Debug, Clone)]
+struct PairIter {
+    // SAFETY INVARIANT: forward.1 and backward.1 are within
+    // b_min..=b_max at all times
+    b_min: RoomCoordinate,
+    b_max: RoomCoordinate,
+    // SAFETY INVARIANT: forward <= backward at all times
+    forward: (RoomCoordinate, RoomCoordinate),
+    backward: (RoomCoordinate, RoomCoordinate),
+    done: bool,
+}
+
+impl PairIter {
+    fn new(min: (RoomCoordinate, RoomCoordinate), max: (RoomCoordinate, RoomCoordinate)) -> Self {
+        Self {
+            b_min: min.1,
+            b_max: max.1,
+            forward: min,
+            backward: max,
+            done: max.0 < min.0 || max.1 < min.1,
+        }
+    }
+}
+
+impl Iterator for PairIter {
+    type Item = (RoomCoordinate, RoomCoordinate);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        }
+
+        let res = Some(self.forward);
+
+        if self.forward == self.backward {
+            self.done = true;
+        } else if self.forward.1 == self.b_max {
+            // SAFETY: self.backward.1 <= self.b_max, so
+            // self.forward.0 < self.backward.0, meaning we can
+            // increment by 1.
+            self.forward = (
+                unsafe { RoomCoordinate::unchecked_new(self.forward.0.u8() + 1) },
+                self.b_min,
+            );
+        } else {
+            // SAFETY: self.forward.1 < self.b_max, so we can step
+            // up by 1.
+            self.forward.1 = unsafe { RoomCoordinate::unchecked_new(self.forward.1.u8() + 1) };
+        }
+
+        res
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.len();
+        (len, Some(len))
+    }
+
+    fn fold<B, F>(self, init: B, mut f: F) -> B
+    where
+        Self: Sized,
+        F: FnMut(B, Self::Item) -> B,
+    {
+        if self.done {
+            return init;
+        }
+
+        if self.forward.0 == self.backward.0 {
+            return range_inclusive(self.forward.1, self.backward.1)
+                .map(|b| (self.forward.0, b))
+                .fold(init, f);
+        }
+
+        let forward_partial_acc = range_inclusive(self.forward.1, self.b_max)
+            .map(|b| (self.forward.0, b))
+            .fold(init, &mut f);
+
+        let middle_partials_acc = range_exclusive(
+            unsafe { RoomCoordinate::unchecked_new(self.forward.0.u8() + 1) },
+            self.backward.0,
+        )
+        .fold(forward_partial_acc, |inner_acc, a| {
+            range_inclusive(self.b_min, self.b_max)
+                .map(|b| (a, b))
+                .fold(inner_acc, &mut f)
+        });
+
+        range_inclusive(self.b_min, self.backward.1)
+            .map(|b| (self.backward.0, b))
+            .fold(middle_partials_acc, f)
+    }
+}
+
+impl FusedIterator for PairIter {}
+
+impl ExactSizeIterator for PairIter {
+    fn len(&self) -> usize {
+        if self.done {
+            return 0;
+        }
+
+        let forward = (self.forward.0.u8(), self.forward.1.u8());
+        let backward = (self.backward.0.u8(), self.backward.1.u8());
+
+        if forward.0 == backward.0 {
+            return (backward.1 - forward.1 + 1) as usize;
+        }
+
+        let full_ranges = (backward.0 - forward.0 - 1) as usize;
+        full_ranges * (self.b_max.u8() - self.b_min.u8() + 1) as usize
+            + (self.b_max.u8() - forward.1 + 1) as usize
+            + (backward.1 - self.b_min.u8() + 1) as usize
+    }
+}
+
+impl DoubleEndedIterator for PairIter {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        }
+
+        let res = Some(self.backward);
+
+        if self.backward == self.forward {
+            self.done = true;
+        } else if self.backward.1 == self.b_min {
+            // SAFETY: self.forward.1 >= self.b_min, so
+            // self.forward.0 < self.backward.0, meaning we can
+            // decrement by 1.
+            self.backward = (
+                unsafe { RoomCoordinate::unchecked_new(self.backward.0.u8() + 1) },
+                self.b_max,
+            );
+        } else {
+            // SAFETY: self.backward.1 > self.b_min, so we can step
+            // down by 1.
+            self.backward.1 = unsafe { RoomCoordinate::unchecked_new(self.backward.1.u8() + 1) };
+        }
+
+        res
+    }
+
+    fn rfold<B, F>(self, init: B, mut f: F) -> B
+    where
+        Self: Sized,
+        F: FnMut(B, Self::Item) -> B,
+    {
+        if self.done {
+            return init;
+        }
+
+        if self.forward.0 == self.backward.0 {
+            return range_inclusive(self.forward.1, self.backward.1)
+                .map(|b| (self.forward.0, b))
+                .rfold(init, f);
+        }
+
+        let backward_partial_acc = range_inclusive(self.b_min, self.backward.1)
+            .map(|b| (self.backward.0, b))
+            .rfold(init, &mut f);
+
+        let middle_partials_acc = range_exclusive(
+            unsafe { RoomCoordinate::unchecked_new(self.forward.0.u8() + 1) },
+            self.backward.0,
+        )
+        .rfold(backward_partial_acc, |inner_acc, a| {
+            range_inclusive(self.b_min, self.b_max)
+                .map(|b| (a, b))
+                .rfold(inner_acc, &mut f)
+        });
+
+        range_inclusive(self.forward.0, self.b_max)
+            .map(|b| (self.forward.0, b))
+            .rfold(middle_partials_acc, f)
+    }
+}
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Order {
@@ -8,33 +187,25 @@ pub enum Order {
     RowMajor,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct GridIter {
-    b_min: u8,
-    b_max: u8,
-    forward: (u8, u8),
-    backward: (u8, u8),
+    inner: PairIter,
     order: Order,
-    done: bool,
 }
 
 impl GridIter {
     pub fn new(top_left: RoomXY, bottom_right: RoomXY, order: Order) -> Self {
-        let top = top_left.y.u8();
-        let bottom = bottom_right.y.u8();
-        let left = top_left.x.u8();
-        let right = bottom_right.x.u8();
+        let top = top_left.y;
+        let bottom = bottom_right.y;
+        let left = top_left.x;
+        let right = bottom_right.x;
         let (a_min, a_max, b_min, b_max) = match order {
             Order::ColumnMajor => (left, right, top, bottom),
             Order::RowMajor => (top, bottom, left, right),
         };
         Self {
-            b_min,
-            b_max,
-            forward: (a_min, b_min),
-            backward: (a_max, b_max),
+            inner: PairIter::new((a_min, b_min), (a_max, b_max)),
             order,
-            done: top > bottom || left > right,
         }
     }
 
@@ -50,34 +221,11 @@ impl Iterator for GridIter {
     type Item = RoomXY;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.done {
-            return None;
-        }
-
-        // SAFETY: self.forward will always be a value in the range
-        // (a_min..=a_max, self.b_min..=self.b_max), which were
-        // obtained from existing RoomXY values passed in Self::new.
-        let res = unsafe {
-            Some(self.get_xy(
-                RoomCoordinate::unchecked_new(self.forward.0),
-                RoomCoordinate::unchecked_new(self.forward.1),
-            ))
-        };
-
-        if self.forward == self.backward {
-            self.done = true;
-        } else if self.forward.1 == self.b_max {
-            self.forward = (self.forward.0 + 1, self.b_min);
-        } else {
-            self.forward.1 += 1;
-        }
-
-        res
+        self.inner.next().map(|(a, b)| self.get_xy(a, b))
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        let len = self.len();
-        return (len, Some(len));
+        self.inner.size_hint()
     }
 
     fn fold<B, F>(self, init: B, mut f: F) -> B
@@ -85,76 +233,13 @@ impl Iterator for GridIter {
         Self: Sized,
         F: FnMut(B, Self::Item) -> B,
     {
-        // SAFETY for all RoomCoordinate::unchecked_new usages:
-        // All the passed in u8 values are derived from forward, backward,
-        // b_min, or b_max. Forward and backward will always be in the range
-        // (a_min..=a_max, b_min..=b_max) established in Self::new, and all
-        // four of those come directly from existing RoomXY instances.
-        if self.done {
-            return init;
-        }
-
-        let a = unsafe { RoomCoordinate::unchecked_new(self.forward.0) };
-
-        if self.forward.0 == self.backward.0 {
-            return (self.forward.1..=self.backward.1)
-                .map(|b| self.get_xy(a, unsafe { RoomCoordinate::unchecked_new(b) }))
-                .fold(init, f);
-        }
-
         match self.order {
-            Order::ColumnMajor => {
-                let first_column_acc = {
-                    let x = a;
-                    (self.forward.1..=self.b_max)
-                        .map(|y_raw| unsafe { RoomCoordinate::unchecked_new(y_raw) })
-                        .map(|y| RoomXY { x, y })
-                        .fold(init, &mut f)
-                };
-
-                let middle_columns_acc = (self.forward.0 + 1..self.backward.0)
-                    .map(|x_raw| unsafe { RoomCoordinate::unchecked_new(x_raw) })
-                    .fold(first_column_acc, |inner_acc, x| {
-                        (self.b_min..=self.b_max)
-                            .map(|y_raw| unsafe { RoomCoordinate::unchecked_new(y_raw) })
-                            .map(|y| RoomXY { x, y })
-                            .fold(inner_acc, &mut f)
-                    });
-
-                {
-                    let x = unsafe { RoomCoordinate::unchecked_new(self.backward.0) };
-                    (self.b_min..=self.backward.1)
-                        .map(|y_raw| unsafe { RoomCoordinate::unchecked_new(y_raw) })
-                        .map(|y| RoomXY { x, y })
-                        .fold(middle_columns_acc, f)
-                }
-            }
-            Order::RowMajor => {
-                let first_row_acc = {
-                    let y = a;
-                    (self.forward.1..=self.b_max)
-                        .map(|x_raw| unsafe { RoomCoordinate::unchecked_new(x_raw) })
-                        .map(|x| RoomXY { x, y })
-                        .fold(init, &mut f)
-                };
-
-                let middle_rows_acc = (self.forward.0 + 1..self.backward.0)
-                    .map(|y_raw| unsafe { RoomCoordinate::unchecked_new(y_raw) })
-                    .fold(first_row_acc, |inner_acc, y| {
-                        (self.b_min..=self.b_max)
-                            .map(|x_raw| unsafe { RoomCoordinate::unchecked_new(x_raw) })
-                            .map(|x| RoomXY { x, y })
-                            .fold(inner_acc, &mut f)
-                    });
-
-                {
-                    let y = unsafe { RoomCoordinate::unchecked_new(self.backward.0) };
-                    (self.b_min..=self.backward.1)
-                        .map(|x_raw| unsafe { RoomCoordinate::unchecked_new(x_raw) })
-                        .map(|x| RoomXY { x, y })
-                        .fold(middle_rows_acc, f)
-                }
-            }
+            Order::ColumnMajor => self
+                .inner
+                .fold(init, move |acc, (x, y)| f(acc, RoomXY { x, y })),
+            Order::RowMajor => self
+                .inner
+                .fold(init, move |acc, (y, x)| f(acc, RoomXY { x, y })),
         }
     }
 }
@@ -163,123 +248,27 @@ impl FusedIterator for GridIter {}
 
 impl ExactSizeIterator for GridIter {
     fn len(&self) -> usize {
-        if self.done {
-            return 0;
-        }
-
-        if self.forward.0 == self.backward.0 {
-            return (self.backward.1 - self.forward.1 + 1) as usize;
-        }
-
-        let full_ranges = (self.backward.0 - self.forward.0 - 1) as usize;
-        full_ranges * (self.b_max - self.b_min + 1) as usize
-            + (self.b_max - self.forward.1 + 1) as usize
-            + (self.backward.1 - self.b_min + 1) as usize
+        self.inner.len()
     }
 }
 
 impl DoubleEndedIterator for GridIter {
     fn next_back(&mut self) -> Option<Self::Item> {
-        if self.done {
-            return None;
-        }
-
-        // SAFETY: self.backward will always be a value in the range
-        // (a_min..=a_max, self.b_min..=self.b_max), which were
-        // obtained from existing RoomXY values passed in Self::new.
-        let res = unsafe {
-            Some(self.get_xy(
-                RoomCoordinate::unchecked_new(self.backward.0),
-                RoomCoordinate::unchecked_new(self.backward.1),
-            ))
-        };
-
-        if self.backward == self.forward {
-            self.done = true;
-        } else if self.backward.1 == self.b_min {
-            self.backward = (self.backward.0 - 1, self.b_max);
-        } else {
-            self.backward.1 -= 1;
-        }
-
-        res
+        self.inner.next_back().map(|(a, b)| self.get_xy(a, b))
     }
 
-    fn rfold<B, F>(self, init: B, mut f: F) -> B
+    fn rfold<B, F>(mut self, init: B, mut f: F) -> B
     where
         Self: Sized,
         F: FnMut(B, Self::Item) -> B,
     {
-        // SAFETY for all RoomCoordinate::unchecked_new usages:
-        // All the passed in u8 values are derived from forward, backward,
-        // b_min, or b_max. Forward and backward will always be in the range
-        // (a_min..=a_max, b_min..=b_max) established in Self::new, and all
-        // four of those come directly from existing RoomXY instances.
-        if self.done {
-            return init;
-        }
-
-        let a = unsafe { RoomCoordinate::unchecked_new(self.backward.0) };
-
-        if self.backward.0 == self.forward.0 {
-            return (self.forward.1..=self.backward.1)
-                .map(|b| self.get_xy(a, unsafe { RoomCoordinate::unchecked_new(b) }))
-                .rfold(init, f);
-        }
-
         match self.order {
-            Order::ColumnMajor => {
-                let last_column_acc = {
-                    let x = a;
-                    (self.b_min..=self.backward.1)
-                        .map(|y_raw| unsafe { RoomCoordinate::unchecked_new(y_raw) })
-                        .map(|y| RoomXY { x, y })
-                        .rfold(init, &mut f)
-                };
-
-                let middle_columns_acc = (self.forward.0 + 1..self.backward.0)
-                    .map(|x_raw| unsafe { RoomCoordinate::unchecked_new(x_raw) })
-                    .rfold(last_column_acc, |inner_acc, x| {
-                        (self.b_min..=self.b_max)
-                            .map(|y_raw| unsafe { RoomCoordinate::unchecked_new(y_raw) })
-                            .map(|y| RoomXY { x, y })
-                            .rfold(inner_acc, &mut f)
-                    });
-
-                {
-                    let x = unsafe { RoomCoordinate::unchecked_new(self.forward.0) };
-                    (self.forward.1..=self.b_max)
-                        .map(|y_raw| unsafe { RoomCoordinate::unchecked_new(y_raw) })
-                        .map(|y| RoomXY { x, y })
-                        .rfold(middle_columns_acc, f)
-                }
-            }
-            Order::RowMajor => {
-                let last_row_acc = {
-                    let y = a;
-                    (self.b_min..=self.backward.1)
-                        .map(|x_raw| unsafe { RoomCoordinate::unchecked_new(x_raw) })
-                        .map(|x| RoomXY { x, y })
-                        .rfold(init, &mut f)
-                };
-
-                let middle_rows_acc = (self.forward.0 + 1..self.backward.0)
-                    .map(|y_raw| unsafe { RoomCoordinate::unchecked_new(y_raw) })
-                    .rfold(last_row_acc, |inner_acc, y| {
-                        (self.b_min..=self.b_max)
-                            .map(|x_raw| unsafe { RoomCoordinate::unchecked_new(x_raw) })
-                            .map(|x| RoomXY { x, y })
-                            .rfold(inner_acc, &mut f)
-                    });
-
-                {
-                    let y = unsafe { RoomCoordinate::unchecked_new(self.forward.0) };
-                    (self.forward.1..=self.b_max)
-                        .map(|x_raw| unsafe { RoomCoordinate::unchecked_new(x_raw) })
-                        .map(|x| RoomXY { x, y })
-                        .rfold(middle_rows_acc, f)
-                }
-            }
+            Order::ColumnMajor => self
+                .inner
+                .rfold(init, move |acc, (x, y)| f(acc, RoomXY { x, y })),
+            Order::RowMajor => self
+                .inner
+                .rfold(init, move |acc, (y, x)| f(acc, RoomXY { x, y })),
         }
     }
 }
